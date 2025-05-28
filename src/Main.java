07fc217fbb092e35ae0dcc65307562190dfb1592
NguyenVinhHungSE <vinhung06vl@gmail.com>
2025-05-28 10:38:04 +0700

    new

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,6 @@ public class Main {
                 if(students[i].getMark() < students[j].getMark()){
                     Student x = students[i];
                     students[i] = students[j];
-                    students[j] = x;
                 }
             }
         }
